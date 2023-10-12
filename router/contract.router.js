d5399250772129146d7831d21c1b26ac422870e7
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-10-13 00:07:10 +0800

    [Contract] added contract endpoints

--- router/contract.router.js
+++ router/contract.router.js
@@ -1,8 +1,118 @@
 const express = require("express");
 const router = express.Router();
+const crypto = require("crypto");
 
-router.get("/", async (req, res) => {
-  res.send("contract");
+const token_verification = require("../middleware/token_verification");
+
+const { Web3 } = require("web3");
+const { abi, networks } = require("../SupplyChainNetwork.json");
+const url = "http://127.0.0.1:7545";
+const provider = new Web3.providers.HttpProvider(url);
+const web3 = new Web3(provider);
+const contract = new web3.eth.Contract(abi, networks[5777].address);
+
+router.post("/", token_verification, async (req, res) => {
+  if (!req.body.to)
+    return res.status(400).json({
+      message: "to address does not exist in body",
+    });
+  if (!req.body.product_id)
+    return res.status(400).json({
+      message: "product ID does not exist in body",
+    });
+  const id = parseInt(crypto.randomBytes(2).toString("hex"), 16);
+  try {
+    await contract.methods
+      .sendContract({
+        id,
+        from: req.wallet_address,
+        to: req.body.to,
+        productId: req.body.product_id,
+      })
+      .send({ from: req.wallet_address, gas: "6721975" });
+    return res.status(200).json({
+      message: "contract has been sent",
+      data: [],
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+router.post("/approve", token_verification, async (req, res) => {
+  if (!req.body.id)
+    return res.status(400).json({
+      message: "id does not exist in body",
+    });
+  if (!req.body.from)
+    return res.status(400).json({
+      message: "from address does not exist in body",
+    });
+  if (!req.body.to)
+    return res.status(400).json({
+      message: "to address does not exist in body",
+    });
+  if (!req.body.product_id)
+    return res.status(400).json({
+      message: "product ID does not exist in body",
+    });
+  try {
+    await contract.methods
+      .approveContract({
+        id: req.body.id,
+        from: req.body.from,
+        to: req.body.to,
+        productId: req.body.product_id,
+      })
+      .send({ from: req.wallet_address, gas: "6721975" });
+    res.status(200).json({
+      message: "contract has been approved",
+      data: [],
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+router.post("/decline", token_verification, async (req, res) => {
+  if (!req.body.id)
+    return res.status(400).json({
+      message: "id does not exist in body",
+    });
+  if (!req.body.from)
+    return res.status(400).json({
+      message: "from address does not exist in body",
+    });
+  if (!req.body.to)
+    return res.status(400).json({
+      message: "to address does not exist in body",
+    });
+  if (!req.body.product_id)
+    return res.status(400).json({
+      message: "product ID does not exist in body",
+    });
+  try {
+    await contract.methods
+      .declineContract({
+        id: req.body.id,
+        from: req.body.from,
+        to: req.body.to,
+        productId: req.body.product_id,
+      })
+      .send({ from: req.wallet_address, gas: "6721975" });
+    res.status(200).json({
+      message: "contract has been declined",
+      data: [],
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = router;
