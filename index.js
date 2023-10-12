81226b384ab0fa42ac6b22e6c104a62d05d68198
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-10-12 16:23:35 +0800

    [Authentication] setup cookie authentication

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv").config();
 const { Web3 } = require("web3");
@@ -19,8 +20,23 @@ const request_router = require("./router/request.router");
 const supply_router = require("./router/supply.router");
 const user_router = require("./router/user.router");
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/company", company_router);
 app.use("/contract", contract_router);
