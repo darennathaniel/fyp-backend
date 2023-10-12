59a9b46548645342037a232205cc5de6408d4688
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-10-12 20:04:29 +0800

    [Index] remove any unnecessary codes

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv").config();
-const { Web3 } = require("web3");
-const { abi, networks } = require("./SupplyChainNetwork.json");
 
 //For env File
 const app = express();
 const port = process.env.PORT || 4000;
-const url = "http://127.0.0.1:7545";
-const provider = new Web3.providers.HttpProvider(url);
-const web3 = new Web3(provider);
-const contract = new web3.eth.Contract(abi, networks[5777].address);
 
 const company_router = require("./router/company.router");
 const contract_router = require("./router/contract.router");
@@ -44,15 +37,6 @@ app.use("/product", product_router);
 app.use("/request", request_router);
 app.use("/supply", supply_router);
 app.use("/user", user_router);
-
-// app.get("/", async (req, res) => {
-//   const accounts = await web3.eth.getAccounts();
-//   const contract = new web3.eth.Contract(abi, networks[5777].address);
-//   console.log(
-//     await contract.methods.getCompany(accounts[0]).call({ from: accounts[0] })
-//   );
-//   res.send(`Welcome to Express & TypeScript Server, ${accounts[0]}`);
-// });
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
