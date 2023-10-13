33d005a76e7bd3e8f3686100121dfdaa845d84af
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-10-13 20:21:05 +0800

    [Product] add get product and deserializer

--- router/product.router.js
+++ router/product.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const crypto = require("crypto");
 
 const token_verification = require("../middleware/token_verification");
+const product_deserializer = require("../utils/product_deserializer");
 
 const { Web3 } = require("web3");
 const { abi, networks } = require("../SupplyChainNetwork.json");
@@ -10,6 +11,56 @@ const url = "http://127.0.0.1:7545";
 const provider = new Web3.providers.HttpProvider(url);
 const web3 = new Web3(provider);
 const contract = new web3.eth.Contract(abi, networks[5777].address);
+
+router.get("/", async (req, res) => {
+  if (req.query.product_id) {
+    const product_id = req.query.product_id;
+    try {
+      const product = await contract.methods.listOfProducts(product_id).call();
+      const all_company_length = await contract.methods
+        .getProductOwnerLength(product_id)
+        .call();
+      let companies = [];
+      for (let i = 0; i < all_company_length; i++) {
+        const company_address = await contract.methods
+          .productOwners(product_id, i)
+          .call();
+        const company = await contract.methods
+          .companies(company_address)
+          .call();
+        companies.push({
+          owner: company.owner,
+          name: company.name,
+        });
+      }
+      return res.status(200).json({
+        message: "product retrieved",
+        data: [{ product: product_deserializer(product), companies }],
+      });
+    } catch (err) {
+      return res.status(400).json({
+        message: err.message,
+      });
+    }
+  }
+  try {
+    const all_product_length = await contract.methods.getProductLength().call();
+    let products = [];
+    for (let i = 0; i < all_product_length; i++) {
+      products.push(
+        product_deserializer(await contract.methods.products(i).call())
+      );
+    }
+    return res.status(200).json({
+      message: "all product retrieved",
+      data: [products],
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+});
 
 router.post("/", token_verification, async (req, res) => {
   if (!req.body.product_name)
@@ -24,8 +75,36 @@ router.post("/", token_verification, async (req, res) => {
     return res.status(400).json({
       message: "quantity prerequisite supplies does not exist in body",
     });
+  const prerequisite_supplies = req.body.prerequisite_supplies.map((supply) => {
+    return {
+      productId: supply.product_id,
+      productName: supply.product_name,
+      exist: true,
+    };
+  });
   const id = parseInt(crypto.randomBytes(2).toString("hex"), 16);
-  // TODO: with_recipe endpoint
+  try {
+    await contract.methods
+      .addProductWithRecipe(
+        id,
+        req.body.product_name,
+        prerequisite_supplies,
+        req.body.quantity_prerequisite_supplies
+      )
+      .send({
+        from: req.wallet_address,
+        gas: "6721975",
+      });
+    const product = await contract.methods.listOfProducts(id).call();
+    return res.status(200).json({
+      message: "product has been created",
+      data: [product_deserializer(product)],
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
 });
 
 router.post("/no_recipe", token_verification, async (req, res) => {
@@ -49,12 +128,7 @@ router.post("/no_recipe", token_verification, async (req, res) => {
     const product = await contract.methods.listOfProducts(id).call();
     return res.status(200).json({
       message: "product has been created",
-      data: [
-        {
-          product_id: product.productId.toString(),
-          product_name: product.productName,
-        },
-      ],
+      data: [product_deserializer(product)],
     });
   } catch (err) {
     return res.status(400).json({
