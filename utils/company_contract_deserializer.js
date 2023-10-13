d1e314b9a14b368a6103db01a138bc5a87e6db3e
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-10-13 20:20:20 +0800

    [CompanyContractDeserializer] convert id to number

--- utils/company_contract_deserializer.js
+++ utils/company_contract_deserializer.js
@@ -1,6 +1,6 @@
 module.exports = (company_contract) => {
   return {
-    id: company_contract.id,
+    id: Number(company_contract.id),
     productId: Number(company_contract.productId),
     from: company_contract.from,
     to: company_contract.to,
